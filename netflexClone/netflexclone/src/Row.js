910404edee6ecaff38b08592794c0fefb555abb5
kevin3747118 <kevin3747118@outlook.com>
2022-02-22 10:57:10 +0800

    20220222

--- netflexClone/netflexclone/src/Row.js
+++ netflexClone/netflexclone/src/Row.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import './Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [moveis, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
     useEffect(() => {
         // if [], run once when the row loads, and dont run again.
         async function fetchData() {
@@ -16,7 +20,32 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
     // console.log(moveis)
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            // console.log(movie)
+            movieTrailer(movie?.name || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    const videoId = urlParams.get("v")
+                    // console.log(url, videoId)
+                    if (videoId) {
+                        setTrailerUrl(videoId);
+                    }
+                }).catch(error => console.log(error));
+        }
+    }
 
     return (
         <div className="row">
@@ -26,11 +55,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {moveis.map(movie => (
                     <img
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
-            {/* title */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
